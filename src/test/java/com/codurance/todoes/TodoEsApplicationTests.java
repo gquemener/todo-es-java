777f2e93789b06135e6a49523cbad90167fe252b
Gildas Quéméner <gildas.quemener@codurance.com>
2022-11-08 19:03:31 +0100

    Initial commit

---
+++ src/test/java/com/codurance/todoes/TodoEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codurance.todoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

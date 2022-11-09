2392ef47303c52aa3e91aca32b4b746f1a6573fe
Gildas Quéméner <gildas.quemener@codurance.com>
2022-11-09 21:35:47 +0100

    Add missing methods

--- src/main/java/com/codurance/todoes/TodoId.java
+++ src/main/java/com/codurance/todoes/TodoId.java
@@ -1,5 +1,6 @@
 package com.codurance.todoes;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class TodoId {
@@ -11,5 +12,18 @@ public class TodoId {
 
     public static TodoId generate() {
         return new TodoId(UUID.randomUUID());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TodoId todoId = (TodoId) o;
+        return Objects.equals(value, todoId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
     }
 }

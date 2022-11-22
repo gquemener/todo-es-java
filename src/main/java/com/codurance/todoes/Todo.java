da81710681719d8a7e5d4e71554f71eb0136beff
Gildas Quéméner <gildas.quemener@codurance.com>
2022-11-22 09:42:33 +0100

    Display closing duration

--- src/main/java/com/codurance/todoes/Todo.java
+++ src/main/java/com/codurance/todoes/Todo.java
@@ -1,16 +1,28 @@
 package com.codurance.todoes;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
 public final class Todo {
     private final String id;
     private final String description;
-    private final boolean closed;
+    private final Instant openedAt;
+    private Instant closedAt;
 
-    public Todo(String id, String description, boolean closed) {
+    public Todo(String id, String description, Instant openedAt) {
         this.id = id;
         this.description = description;
-        this.closed = closed;
+        this.openedAt = openedAt;
+    }
+
+    public Todo close(Instant closedAt) {
+        Todo todo = new Todo(this.id, this.description, this.openedAt);
+        todo.closedAt = closedAt;
+
+        return todo;
     }
 
     public String getId() {
@@ -21,35 +33,20 @@ public final class Todo {
         return description;
     }
 
-    public boolean closed() {
-        return closed;
-    }
-
     public String getDuration() {
-        return closed ? "y" : null;
+        return closedAt != null ? Duration.between(openedAt, closedAt).toString() : null;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (Todo) obj;
-        return Objects.equals(this.id, that.id) &&
-                Objects.equals(this.description, that.description) &&
-                this.closed == that.closed;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Todo todo = (Todo) o;
+        return id.equals(todo.id) && description.equals(todo.description) && openedAt.equals(todo.openedAt) && Objects.equals(closedAt, todo.closedAt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, description, closed);
+        return Objects.hash(id, description, openedAt, closedAt);
     }
-
-    @Override
-    public String toString() {
-        return "Todo[" +
-                "id=" + id + ", " +
-                "description=" + description + ", " +
-                "closed=" + closed + ']';
-    }
-
 }
--- src/main/java/com/codurance/todoes/TodoProjection.java
+++ src/main/java/com/codurance/todoes/TodoProjection.java
@@ -1,9 +1,12 @@
 package com.codurance.todoes;
 
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.eventhandling.Timestamp;
 import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -12,18 +15,21 @@ public class TodoProjection {
     private final List<Todo> todoList = new CopyOnWriteArrayList<>();
 
     @EventHandler
-    public void on(TodoWasCreated evt) {
-        Todo todo = new Todo(evt.id(), evt.description(), false);
+    public void on(
+        TodoWasCreated evt,
+        @Timestamp Instant occurredAt
+    ) {
+        Todo todo = new Todo(evt.id(), evt.description(), occurredAt);
         todoList.add(todo);
     }
 
     @EventHandler
-    public void on(TodoWasClosed evt) {
+    public void on(TodoWasClosed evt, @Timestamp Instant occurredAt) {
         todoList.stream()
                 .filter(todo -> todo.getId().equals(evt.id()))
                 .findFirst()
                 .ifPresent(todo -> {
-                    Todo updatedTodo = new Todo(todo.getId(), todo.getDescription(), true);
+                    Todo updatedTodo = todo.close(occurredAt);
                     todoList.remove(todo);
                     todoList.add(updatedTodo);
                 });

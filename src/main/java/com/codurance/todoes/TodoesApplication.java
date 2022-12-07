f799cb0d49b2c94ab5201f120d17a5f0f0172e45
Gildas Quéméner <gildas.quemener@codurance.com>
2022-12-07 07:36:45 +0100

    Use JPAEventStorageEngine
    See JpaEventStoreAutoConfiguration

--- src/main/java/com/codurance/todoes/TodoesApplication.java
+++ src/main/java/com/codurance/todoes/TodoesApplication.java
@@ -1,12 +1,7 @@
 package com.codurance.todoes;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.axonframework.serialization.Serializer;
-import org.axonframework.serialization.json.JacksonSerializer;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "com.codurance.todoes")
 public class TodoesApplication {
